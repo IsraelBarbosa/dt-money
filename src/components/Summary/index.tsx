f97c49430ed58acfc0c367f54ba44f395c306649
Israel Barbosa <medeirosisrael@outlook.com>
2024-04-01 13:21:50 -0300

    bug no array do summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,70 +2,87 @@ import { Container, SummaryContainer } from './styles';
 import arrowCircleUpRegular from '../../assets/arrow-circle-up-regular.svg';
 import arrowCircleDownRegular from '../../assets/arrow-circle-down-regular.svg';
 import currencyDollarRegular from '../../assets/currency-dollar-regular.svg';
-import { useTransactions } from '../../hooks/useTransactions';
+import { Transaction, useTransactions } from '../../hooks/useTransactions';
 
 export function Summary() {
   const { transactions } = useTransactions();
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'deposit') {
-        acc.deposits += transaction.amount;
-        acc.total += transaction.amount;
-      } else {
-        acc.withdraws += transaction.amount;
-        acc.total -= transaction.amount;
-      }
+  let summary = {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  };
 
-      return acc;
-    },
-    {
-      deposits: 0,
-      withdraws: 0,
-      total: 0,
-    }
-  );
+  if (Array.isArray(transactions)) {
+    summary = transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'deposit') {
+          acc.deposits += transaction.amount;
+          acc.total += transaction.amount;
+        } else {
+          acc.withdraws += transaction.amount;
+          acc.total -= transaction.amount;
+        }
+
+        return acc;
+      },
+      {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+      }
+    );
+
+    console.log(transactions, 'transactions');
+    console.log(summary, 'summary');
+  }
 
   return (
     <Container>
       <SummaryContainer>
-        <div>
-          <header>
-            <p>Entradas</p>
-            <img src={arrowCircleUpRegular} alt="Entradas" />
-          </header>
-          <strong>
-            {new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(summary.deposits)}
-          </strong>
-        </div>
-        <div>
-          <header>
-            <p>Saídas</p>
-            <img src={arrowCircleDownRegular} alt="Saídas" />
-          </header>
-          <strong>
-            -{' '}
-            {new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(summary.withdraws)}
-          </strong>
-        </div>
-        <div className="highlight-background">
-          <header>
-            <p>Total</p>
-            <img src={currencyDollarRegular} alt="Total" />
-          </header>
-          <strong>
-            {new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(summary.total)}
-          </strong>
-        </div>
+        {Array.isArray(transactions) ? (
+          <>
+            <div>
+              <header>
+                <p>Entradas</p>
+                <img src={arrowCircleUpRegular} alt="Entradas" />
+              </header>
+              <strong>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(summary.deposits)}
+              </strong>
+            </div>
+            <div>
+              <header>
+                <p>Saídas</p>
+                <img src={arrowCircleDownRegular} alt="Saídas" />
+              </header>
+              <strong>
+                -{' '}
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(summary.withdraws)}
+              </strong>
+            </div>
+            <div className="highlight-background">
+              <header>
+                <p>Total</p>
+                <img src={currencyDollarRegular} alt="Total" />
+              </header>
+              <strong>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(summary.total)}
+              </strong>
+            </div>
+          </>
+        ) : (
+          <p>Dados inválidos</p>
+        )}
       </SummaryContainer>
     </Container>
   );
